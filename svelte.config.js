2a8a51c85baea4376324c96b817cae21edf9189f
exadprinter <sihem.bouhenniche@inria.fr>
2025-12-16 12:28:13 +0100

    fix config

--- svelte.config.js
+++ svelte.config.js
@@ -5,34 +5,28 @@ import { mdsvex, escapeSvelte } from 'mdsvex';
 import { createHighlighter } from 'shiki';
 
 const mdsvexOptions = {
-  extensions: ['.md'],
-  highlight: {
-    highlighter: async (code, lang = 'text') => {
-      const highlighter = await createHighlighter({
-        themes: ['vesper'],
-        langs: ['javascript', 'typescript', 'svelte', 'py', 'python']
-      });
-      await highlighter.loadLanguage('javascript', 'typescript', 'svelte', 'py', 'python');
-      await highlighter.loadTheme('vesper');
-      const html = escapeSvelte(
-        highlighter.codeToHtml(code, { lang, theme: 'vesper' })
-      );
-      return `{@html \`${html}\` }`;
-    }
-  }
+	extensions: ['.md'],
+	highlight: {
+		highlighter: async (code, lang = 'text') => {
+			const highlighter = await createHighlighter({
+				themes: ['vesper'],
+				langs: ['javascript', 'typescript', 'svelte', 'py', 'python']
+			});
+			await highlighter.loadLanguage('javascript', 'typescript', 'svelte', 'py', 'python');
+			await highlighter.loadTheme('vesper');
+			const html = escapeSvelte(highlighter.codeToHtml(code, { lang, theme: 'vesper' }));
+			return `{@html \`${html}\` }`;
+		}
+	}
 };
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  extensions: ['.svelte', '.md'],
-  preprocess: [vitePreprocess(), mdsvex(mdsvexOptions)],
+	extensions: ['.svelte', '.md'],
+	preprocess: [vitePreprocess(), mdsvex(mdsvexOptions)],
 
-  kit: {
-    adapter: adapter(),
-    prerender: {
-      default: true
-    }
-  }
+	kit: {
+		adapter: adapter()
+	}
 };
 
 export default config;
