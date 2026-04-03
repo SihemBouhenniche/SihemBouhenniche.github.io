30eb9acc627f353d0523026d49d7fc659c3e7b3a
Bouhenniche Sihem <32648972+SihemBouhenniche@users.noreply.github.com>
2026-04-03 09:37:50 +0200

    Update PublicationCard.svelte

--- src/lib/components/portfolio/PublicationCard.svelte
+++ src/lib/components/portfolio/PublicationCard.svelte
@@ -8,52 +8,41 @@
 	export let company: string = '';
 	export let title: string = '';
 	export let subtitle: string = '';
-	export let href: string = '';
-	export let badges: string[]=[''];
 	export let description: string = '';
-	let isExpanded = false;
-
-	let handleClick = (e: MouseEvent) => {
-		if (description) {
-			e.preventDefault();
-			isExpanded = !isExpanded;
-		}
-	};
+	export let links: readonly {
+			icon?: any;
+			title: string;
+			href: string;
+		}[] = [];
 </script>
 
-<a href={href || '#'} on:click={handleClick}>
-	<div class="flex rounded-lg bg-card text-card-foreground">
-		<div class="group ml-4 flex-grow flex-col items-center">
-			<div class="flex flex-col">
-				<div class="flex items-center justify-between gap-x-2 text-base">
-					<h3
-						class="inline-flex items-center justify-center text-xs font-semibold leading-none sm:text-sm"
-					>
-						{company}
-						{#if badges?.length > 0 && badges[0] !== ''}
-							<span class="inline-flex gap-x-1">
-								{#each badges as badge, index}
-									<Badge variant="secondary" class="align-middle text-xs" key={index}>
-										{badge}
-									</Badge>
-								{/each}
-							</span>
-						{/if}
-						<ChevronRightIcon
-							class={cn(
-								'size-4 translate-x-0 transform opacity-0 transition-all duration-300 ease-out group-hover:translate-x-1 group-hover:opacity-100',
-								isExpanded ? 'rotate-90' : 'rotate-0'
-							)}
-						/>
-					</h3>
-				</div>
-				{#if title}
-					<div class="font-sans text-xs">{@html title}</div>
-				{/if}
+<div class="flex rounded-lg bg-card text-card-foreground">
+	<div class="group ml-4 flex-grow flex-col items-center">
+		<div class="flex flex-col">
+			<div class="flex items-center justify-between gap-x-2 text-base">
+				<h3 class="inline-flex items-center justify-center text-xs font-semibold leading-none sm:text-sm">
+					{company}
+						
+				</h3>
 			</div>
-			{#if description}
-				<div class="mt-2 text-xs sm:text-sm"> {description} </div>
+			{#if title}
+				<div class="font-sans text-xs">{@html title}</div>
 			{/if}
 		</div>
+		{#if description}
+			<div class="mt-2 text-xs sm:text-sm"> {description} </div>
+		{/if}
+		{#if links && links.length > 0}
+			<div class="mt-2 flex flex-row flex-wrap items-start gap-2">
+				{#each links as link, idx}
+					<a href={link.href}>
+						<Badge key={idx} title={link.title} class="flex gap-2">
+							<svelte:component this={link.icon} class="h-4 w-4 " strokeWidth={1.6} />
+								{link.title}
+						</Badge>
+					</a>
+				{/each}
+			</div>
+		{/if}
 	</div>
-</a>
+</div>
